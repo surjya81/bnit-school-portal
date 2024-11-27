e36680c08ac76c196ba0b4a97a1192e5e12fb36e
Surjya <surjya189@gmail.com>
2024-11-28 02:14:16 +0530

    change icon and added tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/BNIT PUBLIC SCHOOL/i);
   expect(linkElement).toBeInTheDocument();
 });
